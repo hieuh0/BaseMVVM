05a5f8f470157e60a860727a18cfe2efa07e5894
hieuhd <hieuhd1003@gmail.com>
2019-07-30 10:32:33 +0700

    fix error annotationProcessor

--- app/src/main/java/com/single/projectbasemvvm/MainActivity.java
+++ app/src/main/java/com/single/projectbasemvvm/MainActivity.java
@@ -3,11 +3,30 @@ package com.single.projectbasemvvm;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.single.projectbasemvvm.base.BaseActivity;
+import com.single.projectbasemvvm.base.BaseViewModel;
+
+public class MainActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    public BaseViewModel getViewModel() {
+        return null;
+    }
+
+    @Override
+    protected void setUp() {
+
+    }
+
+    @Override
+    public int getLayoutRes() {
+        return 0;
     }
 }
